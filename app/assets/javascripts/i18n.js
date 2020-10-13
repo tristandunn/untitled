339bda449dd3962ea568b7f0a578851af9ee08ff
Tristan Dunn <tristanzdunn@gmail.com>
2020-10-12 21:29:20 -0500

    Manually require translations to avoid require.context warnings.
    Includes a test to ensure each translation file is required.
    
    Also sorts the translations by key.

--- app/assets/javascripts/i18n.js
+++ app/assets/javascripts/i18n.js
@@ -2,18 +2,9 @@ import I18n from "i18n-js";
 
 I18n.defaultLocale = window.I18n.defaultLocale;
 I18n.locale        = window.I18n.locale;
-
-/* istanbul ignore next */
-I18n.translations  = require.context ?
-  require
-    .context("../../../config/locales/", true, /\.ya?ml$/)
-    .keys()
-    .reduce((result, file) => {
-      return Object.assign(
-        result,
-        require(`../../../config/locales/${file.replace("./", "")}`)
-      );
-    }, {}) :
-  require("../../../config/locales/en.yml");
+I18n.translations  = Object.assign(
+  {},
+  require("../../../config/locales/en.yml")
+);
 
 export default I18n;
--- spec/javascripts/application/i18n.spec.js
+++ spec/javascripts/application/i18n.spec.js
@@ -1,4 +1,6 @@
+import fs from "fs";
 import I18n from "i18n";
+import YAML from "yaml";
 
 describe("I18n", () => {
   it("assigns default locale from global object", () => {
@@ -7,5 +9,17 @@ describe("I18n", () => {
 
   it("assigns locale from global object", () => {
     expect(I18n.locale).to.eq(window.I18n.locale);
+  });
+
+  it("loads all available translations", () => {
+    const files = fs.readdirSync("./config/locales");
+
+    files.forEach((file) => {
+      const translations = YAML.parse(
+        fs.readFileSync(`./config/locales/${file}`).toString()
+      );
+
+      expect(I18n.translations).to.deep.include(translations);
+    });
   });
 });
