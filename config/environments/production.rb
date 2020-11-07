b80396b0c0ed7e34a581db1968f29802bd0902bc
Tristan Dunn <tristanzdunn@gmail.com>
2020-11-07 17:50:41 -0600

    Add caching headers to production configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, max-age=#{1.year.to_i}",
+    "Expires"       => 1.year.from_now.to_formatted_s(:rfc822)
+  }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
