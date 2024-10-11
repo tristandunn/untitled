e34de4f5551999ebc42c11104f517285a6e2c946
Tristan Dunn <tristanzdunn@gmail.com>
2024-10-11 19:20:41 -0400

    Add simple authentication.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,4 +4,8 @@ require "rails_helper"
 
 describe ApplicationController do
   it { is_expected.to be_a(ActionController::Base) }
+
+  it "includes the authentication helpers" do
+    expect(described_class).to include(Authentication)
+  end
 end
