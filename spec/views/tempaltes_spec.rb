c559f7f7e6d4d65c1ec91cb7fdcb6b77ab3779ad
Tristan Dunn <tristanzdunn@gmail.com>
2024-11-12 22:12:02 -0500

    Require explicit locals in view templates.

---
+++ spec/views/tempaltes_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Templates" do
+  let(:paths) { Rails.root.glob("app/views/**/*.*") }
+
+  let(:views) do
+    paths.map do |path|
+      {
+        line: File.open(path, &:readline).strip,
+        path: path.to_s.sub(Rails.root.to_s, "").sub("/", "")
+      }
+    end
+  end
+
+  it "require explicit locals" do
+    expect(views).to all(
+      match(line: /\A<%# locals: \(.*\) %>\z/, path: String)
+    )
+  end
+end
