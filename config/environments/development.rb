00a14b6a5af6c4aa56558fd99d737538c6dc9489
Tristan Dunn <hello@tristandunn.com>
2025-09-06 18:49:18 -0400

    Update dependencies.
    * Updates Node to 24.7.0.
    * Updates Yarn to 4.9.4.
    * Updates factory_bot_rails, puma, ruboocop, rubocop-performance in Ruby.
    * Updates eslint and stylelint in JavaScript.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,12 +15,18 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
-  # Enable caching.
-  config.action_controller.perform_caching = true
-  config.action_controller.enable_fragment_cache_logging = true
-  config.public_file_server.headers = {
-    "cache-control" => "public, max-age=#{2.days.to_i}"
-  }
+  # Enable or disable Action Controller caching, which is disabled by default.
+  #
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join("tmp/caching-dev.txt").exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+    config.public_file_server.headers = {
+      "cache-control" => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+  end
 
   # Change to :null_store to avoid any caching.
   config.cache_store = :solid_cache_store
